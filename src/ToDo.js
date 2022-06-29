987b3093e84667eec165dc3a14a3c41a708a3d2c
Ankit Shaji <ankitshaji@gmail.com>
2022-06-30 04:32:11 +1200

    Create ToDo compnt,ToDoList passes ToDo todo prop

---
+++ src/ToDo.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+//Todo Component gets passed in todo prop
+export default function ToDo({ todo }) {
+  return <div>{todo}</div>;
+}
--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from "react";
+//import ToDo Component
+import ToDo from "./ToDo";
 
 //ToDoList Component receives prop todos
-export default function ToDoList({todos}) {
-  return (
-    <div>{todos.length}</div>
-  )
+export default function ToDoList({ todos }) {
+  //for each todo in todos set it as prop in ToDo component
+  //return ToDo component
+  return todos.map((todo) => {
+    return <ToDo todo={todo} />;
+  });
 }
