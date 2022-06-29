34be2efa6a488110c8912081ebbe1d917a1b594f
Ankit Shaji <ankitshaji@gmail.com>
2022-06-30 01:11:41 +1200

    comment

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+//call app.js + render it into div root
 ReactDOM.render(<App/>,document.getElementById('root'));
